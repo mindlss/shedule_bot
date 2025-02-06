b9a238c18cdd9fd9ce5e0d1a2ea5443e5317163d
Антон Архипов <i.am@mindes.ru>
2025-02-06 20:14:58 +0300

    [+] Minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { Telegraf } from 'telegraf';
 import schedule from 'node-schedule';
-import { getScheduleForDate } from './getSchedule.js';
-import { generateImage } from './generateImage.js';
+import { getScheduleForDate, getScheduleForWeek } from './getSchedule.js';
+import { generateDay } from './generateImage.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,7 +10,7 @@ const { TOKEN, CHAT_ID, THREAD_ID } = process.env;
 const bot = new Telegraf(TOKEN);
 
 // Планирование отправки расписания в 19:00 каждый день, кроме воскресенья
-schedule.scheduleJob({ hour: 19, minute: 0, dayOfWeek: [1, 2, 3, 4, 5, 7] }, async () => {
+schedule.scheduleJob({ hour: 17, minute: 0, dayOfWeek: [1, 2, 3, 4, 5, 7] }, async () => {
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
   
@@ -21,15 +21,33 @@ schedule.scheduleJob({ hour: 19, minute: 0, dayOfWeek: [1, 2, 3, 4, 5, 7] }, asy
     const formattedDate = `${year}-${month}-${day}`;
     const data = await getScheduleForDate(formattedDate);
 
-    await generateImage(data.dateInfo, data.lessons, data.location, data.noPairs, './auto_schedule.png');
+    await generateDay(data.dateInfo, data.lessons, data.location, data.noPairs, './auto_schedule.png');
 
     bot.telegram.sendPhoto(CHAT_ID, { source: './auto_schedule.png' }, { reply_to_message_id: THREAD_ID })
         .then(() => console.log('Расписание отправлено'))
         .catch(console.error);
 });
 
-bot.launch();
-console.log('Бот запущен');
+// bot.launch();
+// console.log('Бот запущен');
 
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+// process.once('SIGINT', () => bot.stop('SIGINT'));
+// process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+
+
+async function test() {
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    const shed = await getScheduleForWeek("2025-02-03")
+
+    console.log(shed)
+}
+
+test()
